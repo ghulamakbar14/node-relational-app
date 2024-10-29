805bf2f02749a406f142ef5e5a7567de999f6d3b
ghulamakbar14 <ghulamakbar14@gmail.com>
2024-10-29 20:25:57 +0500

    apply validation on save and update post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,3 +1,5 @@
+const validator = require('fastest-validator');
+
 const models = require('../models');
 
 function index (reg, res) {
@@ -39,6 +41,21 @@ function update(req, res) {
         categoryId: req.body.categoryId
     };
 
+    const v = new validator();
+    const schema = {
+        title: {type: "string", max: "100", optional: false},
+        content: {type: "string", max: "500", optional: false},
+        categoryId: {type: "number", optional: false},
+        imageUrl: {type: "string", optional: true},
+    };
+    const validatedData = v.validate(updatePost, schema);
+    if(validatedData !== true) {
+        return res.status(400).json({
+            message: "Invalid data sent",
+            error: validatedData
+        })
+    }
+
     models.Post.update(updatePost, {where: {id:id, userId: userId} }).then(result => {
         if(result) {
             res.status(200).json({
@@ -56,6 +73,45 @@ function update(req, res) {
             error: error
         })
     });
+}
+
+function save(req, res) {
+    const v = new validator();
+
+    const post = {
+        title: req.body.title,
+        content: req.body.content,
+        imageUrl: req.body.imageUrl,
+        userId: 1,
+        categoryId: 1
+    };
+
+    const schema = {
+        title: {type: "string", max: "100", optional: false},
+        content: {type: "string", max: "500", optional: false},
+        categoryId: {type: "number", optional: false},
+        imageUrl: {type: "string", optional: true},
+    };
+
+    const validatedData = v.validate(post, schema);
+    if(validatedData !== true) {
+        return res.status(400).json({
+            message: "Invalid data sent",
+            error: validatedData
+        })
+    }
+
+    models.Post.create(post).then(result => {
+        res.status(201).json({
+            message: "Post created successfully",
+            post: result
+        })
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error
+        })
+    })
 }
 
 function destroy(req, res) {
@@ -77,27 +133,6 @@ function destroy(req, res) {
             error: error
         })
     });
-}
-
-function save(req, res) {
-    const post = {
-        title: req.body.title,
-        content: req.body.content,
-        imageUrl: req.body.imageUrl,
-        userId: 1,
-        categoryId: 1
-    };
-    models.Post.create(post).then(result => {
-        res.status(201).json({
-            message: "Post created successfully",
-            post: result
-        })
-    }).catch(error => {
-        res.status(500).json({
-            message: "Something went wrong",
-            error: error
-        })
-    })
 }
 
 module.exports = {
